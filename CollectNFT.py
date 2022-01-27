51fa7685406924b8c33bbf1a889fb55d296ef558
Jpalm20 <96892730+Jpalm20@users.noreply.github.com>
2022-01-26 20:38:33 -0500

    Updated API Key Requirement
    OpenSea API now requires API key for similar request such at this or else it will return an Error 403 (Acknowledged but Denied). I am currently waiting for my own API Key to use this again.

--- CollectNFT.py
+++ CollectNFT.py
@@ -2,9 +2,14 @@ import requests
 import json
 import shutil
 
-url = "https://api.opensea.io/api/v1/assets?owner={WALLET ADDRESS}&order_direction=desc&offset=0&limit=20"
+url = "https://api.opensea.io/api/v1/assets?owner=0xb0439622f93f7349d6307c56a32b0d5aa42b7940&order_direction=desc&offset=0&limit=20"
 
-response = requests.request("GET", url)
+headers = {
+	"Accept": "application/json",
+	"X-API-KEY": ""
+}
+
+response = requests.request("GET", url, headers=headers)
 
 formatted = response.text
 temp = json.loads(formatted)
